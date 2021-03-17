6381fda55fcced38e92174b5f3dffef3b95f4202
ganklah <ganklah@gmail.com>
2021-03-17 19:29:24 +1000

    updated saved variables

--- ShiaBloodlust.lua
+++ ShiaBloodlust.lua
@@ -1,21 +1,22 @@
 -- Author      : Oldtimer
-ShiaBL = LibStub("AceAddon-3.0"):NewAddon("ShiaBL", "AceConsole-3.0", "AceEvent-3.0")
-
---[[
-todo: check buff only on player
+Shiabl = LibStub("AceAddon-3.0"):NewAddon("Shiabl", "AceConsole-3.0", "AceEvent-3.0")
 
 
---]]
-
-
-
-local soundEnabled = true
-local textureEnabled = true
 local lustFrame
 local optionsFrame
 local ag
 
 
+-- local get and set options make life a tead easier
+
+local defaults = {
+	char = {
+	fireballEnabled = true,
+	frostAuraEnabled = true,
+	soundEnabled = true,
+	textureEnabled = true
+	}
+}
 
 
 local triggerAuras = { 
@@ -25,9 +26,14 @@ local triggerAuras = {
 }
 
 
-local options = {
+
+
+function Shiabl:OnInitialize()
+	self.db = LibStub("AceDB-3.0"):New("ShiaDB", defaults)
+	
+	local options = {
 	name = "Shia Labeouf Bloodlust",
-	handler = ShiaBL,
+	handler = Shiabl,
 	type = "group",
 		args = {
 			firstHeader = {
@@ -42,8 +48,8 @@ local options = {
 				desc = "Mute sounds",
 				type = "toggle",
 				width = "full",
-				set = function(info, val) soundEnabled = val end,
-				get = function(info) return soundEnabled end
+				set = function(info, val) self.db.char.soundEnabled = val end,
+				get = function(info) return self.db.char.soundEnabled end
 			},
 			textureEnabled = {
 				order = 2,
@@ -51,8 +57,8 @@ local options = {
 				desc = "Do not show Shia image",
 				type = "toggle",
 				width = "full",
-				set = function(info, val) textureEnabled = val end,
-				get = function(info) return textureEnabled end
+				set = function(info, val) self.db.char.textureEnabled = val end,
+				get = function(info) return self.db.char.textureEnabled end
 			},
 			auraHeader = {
 				order = 3,
@@ -72,8 +78,8 @@ local options = {
 						desc = "Trigger on Frost Armor",
 						type = "toggle",
 						width = "full",
-						set = function(info, val) soundEnabled = val end,
-						get = function(info) return soundEnabled end
+						set = function(info, val) self.db.char.frostAuraEnabled = val end,
+						get = function(info) return self.db.char.frostAuraEnabled end
 					},
 					fireball = {
 						order = 1,
@@ -81,58 +87,62 @@ local options = {
 						desc = "Trigger on Frost Armor",
 						type = "toggle",
 						width = "full",
-						set = function(info, val) soundEnabled = val end,
-						get = function(info) return soundEnabled end
+						set = function(info, val) self.db.char.fireballEnabled = val end,
+						get = function(info) return self.db.char.fireballEnabled end
 					},
 				},
 			}, 
 		},		
 }
 
-function ShiaBL:OnInitialize()
-	LibStub("AceConfig-3.0"):RegisterOptionsTable("ShiaBL", options)
-	self.optionsFrame = LibStub("AceConfigDialog-3.0"):AddToBlizOptions("ShiaBL") --, "ShiaBL")
+	LibStub("AceConfig-3.0"):RegisterOptionsTable("Shiabl", options)
+	self.optionsFrame = LibStub("AceConfigDialog-3.0"):AddToBlizOptions("Shiabl") --, "Shiabl")
 	self:RegisterChatCommand("shiabl", "ChatCommand")
 	self:RegisterChatCommand("sbl", "ChatCommand")
 	self:RegisterChatCommand("shia", "ChatCommand")
-	--ShiaBLDB = LibStub("AceAddon-3.0"):NewAddon("ShiaBLDB")
-
+	
+	
 	--create frame
 	if lustFrame == nil then 
-		ShiaBL:CreateLustFrame()
+		Shiabl:CreateLustFrame()
 	end
 
 end
 
-function ShiaBL:ChatCommand(input)
-	self.optionsFrame = LibStub("AceConfigDialog-3.0"):Open("ShiaBL")
+function Shiabl:ChatCommand(input)
+	self.optionsFrame = LibStub("AceConfigDialog-3.0"):Open("Shiabl")
 end
 
-function ShiaBL:OnEnable()
+function Shiabl:OnEnable()
 
-	ShiaBL:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED", "ShiaBL_OnEvent")
+	Shiabl:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED", "Shiabl_OnEvent")
 
+	--dbase intro stuff here?
+	--if self.db.char.fireballEnabled then
+		
 	
 end
 
-function ShiaBL:OnDisable()
+function Shiabl:OnDisable()
     -- Called when the addon is disabled
 	ag:Stop()
 	lustFrame:Hide()
 end
 
 
-function ShiaBL:ShiaBL_OnEvent()
+function Shiabl:Shiabl_OnEvent()
 
 	local timestamp, subevent, _, sourceGUID, sourceName, sourceFlags, sourceRaidFlags, destGUID, destName, destFlags, destRaidFlags, _, spellName, _, auraType = CombatLogGetCurrentEventInfo()
 	
 	print(subevent)
-
+	print(destName)
+	print(destGUID)
+	print(sourceName)
 	
-	if subevent == "SPELL_AURA_APPLIED" and tContains(triggerAuras, spellName) then
+	if subevent == "SPELL_AURA_APPLIED" and tContains(triggerAuras, spellName) and destGUID == UnitGUID("player") then
 		lustFrame:Show()
 		print(spellName .. " applied.")
-		--PlaySoundFile("Interface\\AddOns\\ShiaBloodlust\\media\\ShiaSound.ogg")
+		--PlaySoundFile("Interface\\AddOns\\Shiabloodlust\\media\\ShiaSound.ogg")
 		ag:Play()
 	end
 	if subevent == "SPELL_AURA_REMOVED" and tContains(triggerAuras, spellName) then
@@ -142,7 +152,7 @@ function ShiaBL:ShiaBL_OnEvent()
 	end
 end
 
-function ShiaBL:CreateLustFrame()
+function Shiabl:CreateLustFrame()
 
 	lustFrame = CreateFrame("Frame", "LustFrame", UIParent)
 	lustFrame:SetHeight(128)
@@ -158,7 +168,7 @@ function ShiaBL:CreateLustFrame()
 	lustFrame:SetClampedToScreen(true)
 
 	local t = lustFrame:CreateTexture(nil,"BACKGROUND")
-	t:SetTexture("Interface\\AddOns\\ShiaBloodlust\\media\\ShiaTexture.tga")
+	t:SetTexture("Interface\\AddOns\\Shiabloodlust\\media\\ShiaTexture.tga")
 	t:SetAlpha(1)
 	t:SetAllPoints(lustFrame)
 	lustFrame.texture = t
